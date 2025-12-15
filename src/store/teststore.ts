65b592f71b63a58921a9bc6bb1cccd4710100604
devcustrom <devcustrom@gmail.com>
2025-12-15 12:38:46 +0500

    Update teststore.ts

--- src/store/teststore.ts
+++ src/store/teststore.ts
@@ -3,7 +3,7 @@ import { ref } from "vue";
 
 export const useTestStore = defineStore('test', () => {
 	const arr = ref([])
-	console.log('test')
+	console.log('test value: 123')//replace 123 with your value
 	return {
 		arr
 	}
