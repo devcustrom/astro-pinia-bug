401c893eacdd9930819e8416bdc2c4970822dc24
devcustrom <devcustrom@gmail.com>
2026-02-14 18:57:16 +0500

    Add Layout and clientRouter

--- src/store/pinia.ts
+++ src/store/pinia.ts
@@ -1,3 +1,35 @@
 import { createPinia } from "pinia";
+import { swapFunctions } from "astro:transitions/client";
 
-export const pinia = createPinia()
+export let pinia = createPinia()
+
+const save = (doc: Document) => {
+	try {
+		if(!doc) return
+		const state = doc.getElementById('pinia-state')?.textContent ?? ''
+		if(!state) return
+		const data = JSON.parse(state)
+		pinia.state.value = data;
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+if (!import.meta.env.SSR) {
+	document.addEventListener("astro:before-preparation", () => {
+		pinia = createPinia()
+	});
+	document.addEventListener("astro:before-swap", (event) => {
+		const doc = event.newDocument;
+		event.swap = () => {
+			save(doc)
+			swapFunctions.deselectScripts(doc);
+			swapFunctions.swapRootAttributes(doc);
+			swapFunctions.swapHeadElements(doc);
+			const restoreFocusFunction = swapFunctions.saveFocus();
+			swapFunctions.swapBodyElement(doc.body, document.body);
+			restoreFocusFunction();
+		}
+	})
+	save(document)
+}
--- src/store/teststore.ts
+++ src/store/teststore.ts
@@ -2,9 +2,9 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useTestStore = defineStore('test', () => {
-	const arr = ref([])
+	const arr = ref<string[]>([])
 	console.log('test value: 123')//replace 123 with your value
 	return {
 		arr
 	}
-}) 
+})
